dec9e71dc8be8f1d99de0c1d95520a1b03b527d3
Pedro <pedrobrandaofl@gmail.com>
2021-06-30 00:16:24 +0100

    added footer to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,7 +49,7 @@ export default function Home({
         <div className={`${utilStyles.ParallaxContent}`}>
           {/* NAVBAR */}
           <Navbar/>
-
+          <Footer/>
           {/* SHOWCASE */}
           <div>
 
