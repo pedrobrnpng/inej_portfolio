98c65047937b5ea1105db104cfa820a91c6139da
Pedro <pedrobrandaofl@gmail.com>
2021-10-02 17:55:52 +0100

    helper components

---
+++ components/shared-components/content-container.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css';
+
+
+export default function ContentContainer() {
+
+  return (
+    <div className={`${styles.contentContainer}`}>
+      
+    </div>
+  )
+}
