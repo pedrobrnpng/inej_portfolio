77b265cea497fb31abfe5ed6fa573e4b9b2ae93f
pedrobrnpng <pedrobrandaofl@gmail.com>
2021-10-06 17:10:18 +0100

    further developed project page

--- data/projects/jackalope.md
+++ data/projects/jackalope.md
@@ -1,6 +1,17 @@
 ---
 title: "Jackalope"
+type: "Animation"
+colaborators: "Alexandra Ribeiro; Beatriz Carneiro"
+sound: "Kate Mercer"
 date: "2020-01-01"
 img: "https://static.wixstatic.com/media/de5a7f_0e4be1a11cdf4b9a9fcb68dbe6505602~mv2.gif"
+videoUrl: "https://vimeo.com/378897992"
 ---
-my dick fell off
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+Duis vulputate elementum ultrices. Mauris vitae lobortis felis, quis luctus est. Duis quis fermentum dolor. 
+
+Pellentesque vel nisi eget purus fringilla lacinia eu ac tellus. Vivamus et lacinia sem. Sed vel arcu magna. 
+
+Ut non velit eget ligula feugiat feugiat.
+              
--- pages/projects/[project].tsx
+++ pages/projects/[project].tsx
@@ -1,14 +1,16 @@
 import ErrorPage from 'next/error'
 import { useRouter } from 'next/dist/client/router';
 import { getAllPosts, getPostByName } from '../../lib/projects'
+import utilStyles from "./project.module.css"
 
 import markdownToHtml from '../../lib/markdownToHtml'
 import { GetStaticPaths } from 'next';
 import Navbar from '../../components/navbar';
 import Footer from '../../components/footer'
+import Vimeo from '@u-wave/react-vimeo'
 
 
-const Post = ({post}) => {
+const Post = ({ post }) => {
 
   const router = useRouter();
   if (!router.isFallback && !post?.project) {
@@ -17,11 +19,76 @@ const Post = ({post}) => {
 
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <div className={`${utilStyles.container}`}>
+        <div className={`${utilStyles.row}`}>
+          <div className={`${utilStyles.title}`}>
+            <h3>{post.title}</h3>
+            <h5>Animation</h5>
+          </div>
+          <div className={`${utilStyles.aboutProject}`}>
 
+            <div>
+              <h5>School Project</h5>
+            </div>
+            <div>
+              <p>
+                <div dangerouslySetInnerHTML={{ __html: post.content }} />
+              </p>
+            </div>
 
-      {post.content}
+            <div className={`${utilStyles.metaRow}`}>
+              {post.colaborators.length > 0 ?
+                <div className={`${utilStyles.colaborators}`}>
+                  <h5>In colaboration with</h5>
+                  {post.colaborators.map((name) => {
+                    return (
+                      <p>{name.trim()}</p>
+                    )
+                  })}
+                  {/* <ul>
+                    {post.colaborators.map((name) => {
+                      return (
+                        <li>{name.trim()}</li>
+                      )
+                    })}
+                  </ul> */}
+                </div>
+                :
+                <div></div>
+              }
 
+              {post.sound.length > 0 ?
+                <div className={`${utilStyles.sound}`}>
+                  <h5>Sound by</h5>
+                  {post.sound.map((name) => {
+                    return (
+                      <p>{name.trim()}</p>
+                    )
+                  })}
+                  {/* <ul>
+                    {post.sound.map((name) => {
+                      return (
+                        <li>{name.trim()}</li>
+                      )
+                    })}
+                  </ul> */}
+                </div>
+                :
+                <div></div>
+              }
+            </div>
+          </div>
+        </div>
+
+        <div className={`${utilStyles.vimeoContainer}`}>
+          <Vimeo
+            video={post.videoUrl}
+            responsive={true}
+          />
+        </div>
+
+      </div>
       <Footer />
     </div>
   )
@@ -36,7 +103,10 @@ export async function getStaticProps({ params }) {
     'title',
     'img',
     'content',
-    'project'
+    'project',
+    'sound',
+    'colaborators',
+    'videoUrl'
   ]);
 
   const content = await markdownToHtml(post.content || '')
@@ -54,7 +124,7 @@ export async function getStaticProps({ params }) {
 
 export const getStaticPaths: GetStaticPaths = async () => {
 
-  try{
+  try {
     const posts = await getAllPosts(['project']);
     const post1 = await Promise.all(posts);
     const paths = post1.map((post) => {
@@ -70,7 +140,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
     }
   } catch (err) {
     console.error(err);
-    return { paths: [], fallback: false}
+    return { paths: [], fallback: false }
   }
 
 }
