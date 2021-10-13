f012cbb7571487aa54af96ce2c7627c45b88c9f5
pedrobrnpng <pedrobrandaofl@gmail.com>
2021-10-13 14:54:28 +0100

    added Landing Page as a component

---
+++ components/landingPage/index.tsx
@@ -0,0 +1,68 @@
+import utilStyles from './landingPage.module.css'
+import Image from 'next/image'
+import { useEffect, useState } from 'react';
+import getWindowDimensions from '../../utils/windowUtils';
+
+
+export default function LandingPage() {
+
+  const [width, setWidth] = useState<number>();
+  const [height, setHeight] = useState<number>();
+
+  useEffect(() => {
+    const { width, height } = getWindowDimensions();
+    setWidth(width);
+    setHeight(height);
+  }, []);
+
+  useEffect(() => {
+    function handleResize() {
+      const { width, height } = getWindowDimensions();
+      setWidth(width);
+      setHeight(height);
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <div className={`${utilStyles.test}`} style={{height: height}}>
+          <div className={`${utilStyles.ParallaxVideo}`}>
+            {width <= 1150 ?
+              <Image
+                className={`${utilStyles.test}`}
+                alt="Background"
+                src="/images/background_.jpg"
+                layout="fill"
+                objectFit="cover"
+                quality={100} />
+              :
+              <Image
+                className={`${utilStyles.test}`}
+                src="/images/background2.gif"
+                alt="Background"
+                layout="fill"
+                objectFit="cover"
+                quality={100} />
+              // <video
+              //   className={`${utilStyles.video}`}
+              //   autoPlay
+              //   muted
+              //   loop
+              //   id='background'
+              // >
+              //   <source src="/videos/background.mp4" type="video/mp4" />
+              // </video>
+            }
+          </div>
+          <div className={`${utilStyles.content} ${utilStyles.videoText}`}>
+            <h1 className={`${utilStyles.videoText}`}>INÊS PINHEIRO</h1>
+            <h6 className={`${utilStyles.videoText2}`}>Animation and Illustration</h6>
+          </div>
+          <div>
+            <a href="#projects" className={`${utilStyles.scrollDown}`}></a>
+          </div>
+        </div>
+  )
+}
