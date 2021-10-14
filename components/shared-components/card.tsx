6429355026e60979f10f43d143f8dd39b60cfc05
pedrobrnpng <pedrobrandaofl@gmail.com>
2021-10-14 23:31:30 +0100

    added animations to project cards

--- components/shared-components/card.tsx
+++ components/shared-components/card.tsx
@@ -1,36 +1,64 @@
 import utilStyles from './styles.module.css'
 import Link from 'next/link'
+import { motion } from 'framer-motion'
+import useAnimationOnScroll from '../../hooks/useAnimationOnScroll';
 
 type Props = {
+  onTopRow: any
   title: string
   project: string
   img: string
   color: string
 }
 
+const variants = {
+  hide: {
+    y: 300,
+    opacity: 0,
+  },
+  show: {
+    y: 0,
+    opacity: 1,
+  },
+};
+
+const transition = {
+  ease: 'easeOut',
+  duration: 0.5,
+};
 
 export default function ProjectCard({
+  onTopRow,
   project,
   img,
   title,
   color
 }: Props) {
-
+  const scrollThreshold = onTopRow ? 0 : 0.5;
+  const [viewRef, animate, setAnimationHasRun] = useAnimationOnScroll('show', scrollThreshold);
   return (
-    <div className={`${utilStyles.card}`}>
-      <Link as={`/projects/${project}`} href={`/projects/[project]`}>
-        <div className={`${utilStyles.container}`}>
-          <div className={`${utilStyles.imageDiv}`}>
-            <img
-              className={`${utilStyles.image}`}
-              src={img}
-              alt={title}
-              title={title}
-            />
-          </div>
+    <div ref={viewRef} className={`${utilStyles.card}`}>
+      <motion.div
+        initial="hide"
+        animate={animate}
+        variants={variants}
+        transition={transition}
+        onAnimationComplete={setAnimationHasRun}
+      >
+        <Link as={`/projects/${project}`} href={`/projects/[project]`}>
+          <div className={`${utilStyles.container}`}>
 
-          <div className={`overlay`} >
-            <style jsx>{`
+            <div className={`${utilStyles.imageDiv}`}>
+              <img
+                className={`${utilStyles.image}`}
+                src={img}
+                alt={title}
+                title={title}
+              />
+            </div>
+
+            <div className={`overlay`} >
+              <style jsx>{`
                 .overlay {
                   position: absolute;
                   top: 0;
@@ -47,13 +75,16 @@ export default function ProjectCard({
                 .overlay:hover{
                   opacity: 1;
                 }
-              `}</style>
-            <div className={`${utilStyles.text}`}>
-              {title}
+              `}
+              </style>
+              <div className={`${utilStyles.text}`}>
+                {title}
+              </div>
             </div>
+
           </div>
-        </div>
-      </Link>
+        </Link>
+      </motion.div>
     </div>
   )
 }
