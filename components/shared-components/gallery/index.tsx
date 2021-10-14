e05a7ea77ba10ede14cf996eba33be73d22dcd05
pedrobrnpng <pedrobrandaofl@gmail.com>
2021-10-14 23:32:27 +0100

    animationn in gallery

--- components/shared-components/gallery/index.tsx
+++ components/shared-components/gallery/index.tsx
@@ -2,6 +2,7 @@ import utilStyles from './showcase.module.css'
 import { Post } from '../../../types/post'
 import ProjectCard from '../card'
 import Masonry from 'react-masonry-css'
+import { motion } from 'framer-motion'
 
 type Props = {
   allPosts: Post[]
@@ -14,8 +15,15 @@ const breakpoints = {
 }
 
 export default function Gallery({ allPosts }: Props) {
-  
-  const colors = ["126, 89, 32, 0.9", "113, 52, 129, 0.9", "243, 222, 138, 0.9", "98, 168, 124, 0.9", "206, 236, 151, 0.9", "244, 179, 147, 0.9"]
+
+  const colors = [
+    "126, 89, 32, 0.9",
+    "113, 52, 129, 0.9",
+    "243, 222, 138, 0.9",
+    "98, 168, 124, 0.9",
+    "206, 236, 151, 0.9",
+    "244, 179, 147, 0.9"
+  ]
 
   function randomInteger() {
     return Math.floor(Math.random() * colors.length);
@@ -27,14 +35,18 @@ export default function Gallery({ allPosts }: Props) {
       className={`${utilStyles.my_masonry_grid}`}
       columnClassName={`${utilStyles.my_masonry_grid_column}`}
     >
-      {allPosts.map((post) => (
-        <ProjectCard
-          title={post.title}
-          project={post.project}
-          img={post.img}
-          color = {colors[randomInteger()]}
-        />
-      ))}
+      {allPosts.map((post, index) => {
+        const onTopRow = index < 3;
+        return (
+          <ProjectCard
+            onTopRow={onTopRow}
+            title={post.title}
+            project={post.project}
+            img={post.img}
+            color={colors[randomInteger()]}
+          />
+        )
+      })}
     </Masonry>
   )
 }
