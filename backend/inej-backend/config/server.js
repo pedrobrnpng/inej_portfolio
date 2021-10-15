90fc5d99fdea87705d0a099ce4eb8c787992bcab
pedrobrnpng <pedrobrandaofl@gmail.com>
2021-10-15 17:05:22 +0100

    separated backend from frontend

---
+++ backend/inej-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '36c384f80cc53ab864953392999799bd'),
+    },
+  },
+});
