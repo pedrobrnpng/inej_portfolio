3b7eafb7bdf07bbcfa34982784c8506b2d449d1b
pedrobrnpng <pedrobrandaofl@gmail.com>
2021-10-19 18:49:42 +0100

    added contact form to about page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image'
 import Layout from "../../components/layout"
 import { motion } from 'framer-motion'
 import SocialIcons from '../../components/shared-components/social-icons'
+import ContactForm from '../../components/contact-form'
 
 export default function AboutMe({
   pageData
@@ -20,37 +21,37 @@ export default function AboutMe({
       <Head>
         <title>About Me</title>
       </Head>
-      <div>
-        <Layout>
-          <div className={`${utilStyles.aboutContainer}`}>
-            <div className={`${utilStyles.row}`}>
-              <motion.div
-                initial={{ x: -200, opacity: 0 }}
-                animate={{ x: 0, opacity: 1 }}
-                transition={{ delay: .4 }}
-                className={`${utilStyles.imageContainer}`}
-              >
-                <Image width={800} height={1000} alt="profile" src={`${pageData.img}`} quality={100} />
-              </motion.div>
-              <motion.div
-                initial={{ x: 200, opacity: 0 }}
-                animate={{ x: 0, opacity: 1 }}
-                transition={{ delay: .4 }}
-                className={`${utilStyles.textContainer}`}
-              >
-                <h3>AbOuT mE</h3>
-                <div dangerouslySetInnerHTML={{ __html: pageData.contentHtml }} />
-                <div className={`${utilStyles.icons}`}>
-                  <div>
-                    <h3>Feel free to contact me</h3>
-                    <SocialIcons />
-                  </div>
-                </div>
-              </motion.div>
-            </div>
+
+      <Layout>
+        <div className={`${utilStyles.aboutContainer}`}>
+          <div className={`${utilStyles.row}`}>
+            <motion.div
+              initial={{ x: -200, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ delay: .4 }}
+              className={`${utilStyles.imageContainer}`}
+            >
+              <Image width={800} height={1000} alt="profile" src={`${pageData.img}`} quality={100} />
+            </motion.div>
+            <motion.div
+              initial={{ x: 200, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ delay: .4 }}
+              className={`${utilStyles.textContainer}`}
+            >
+              <h3>AbOuT mE</h3>
+              <div dangerouslySetInnerHTML={{ __html: pageData.contentHtml }} />
+            </motion.div>
           </div>
-        </Layout>
+          <ContactForm />
+
+        </div>
+
+      </Layout>
+      <div>
+
       </div>
+
     </div>
   )
 }
