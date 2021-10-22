d862b97a9b89fb01ffbfd0824e7c34f05837e5b8
pedrobrnpng <pedrobrandaofl@gmail.com>
2021-10-22 02:10:52 +0100

    made icon rendering more manageable

--- components/index-contact/index.tsx
+++ components/index-contact/index.tsx
@@ -1,6 +1,30 @@
 import utilStyles from './contact.module.css'
 import { FaInstagram, FaVimeoV, FaWpforms } from 'react-icons/fa'
 
+const socials = [
+  {
+    id: 'instagram',
+    Icon: FaInstagram,
+    href: 'https://www.instagram.com/inej.png/'
+  },
+  {
+    id: 'vimeo',
+    Icon: FaVimeoV,
+    href: 'https://vimeo.com/inespinheiro'
+  },
+  {
+    id: 'curriculum',
+    Icon: FaWpforms,
+    href: 'https://8cb1e5e5-3042-4c42-95ff-64c690ee536b.filesusr.com/ugd/de5a7f_c0834ed978d645babfb863eff3881d6e.pdf'
+  }
+]
+
+const email = {
+  id: 'email',
+  href: 'mailto:inej.pinheiro@gmail.com',
+  text: 'inej.pinheiro@gmail.com'
+}
+
 export default function IndexContact() {
 
   return (
@@ -15,15 +39,23 @@ export default function IndexContact() {
           my Curriculum
         </p>
         <a
-          href="mailto:inej.pinheiro@gmail.com"
+          key={`index-email`}
+          href={email.href}
         >
-          inej.png@gmail.com
+          {email.text}
         </a>
       </div>
       <div className={`${utilStyles.socials}`}>
-        <a> <FaVimeoV /> </a>
-        <a> <FaInstagram /> </a>
-        <a> <FaWpforms /> </a>
+        {socials.map(({ id, href, Icon }) => {
+          return (
+            <a
+              key={`social-index-${id}`}
+              href={href}
+            >
+              <Icon />
+            </a>
+          )
+        })}
       </div>
     </>
   )
