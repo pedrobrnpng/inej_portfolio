3b192c08e371a8e2ae9f5190e3c8c3c264c82d23
pedrobrnpng <pedrobrandaofl@gmail.com>
2021-10-22 02:12:37 +0100

    changed layout of the index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,9 +40,37 @@ export default function Home({ allPosts }: Props) {
           </style>
           <div>
 
+
+
             {/* SHOWCASE */}
-            <div className={`centerPage content`}>
-              <Gallery allPosts={allPosts} />
+            <div className={`content`}>
+              <div className={`centerPage ${utilStyles.showReel}`}>
+                <h1>Showreel</h1>
+                <div style={{ width: "100%", paddingBottom: "50px" }}>
+                  <Vimeo
+                    video={"https://vimeo.com/543139546"}
+                    dnt={true}
+                    showPortrait={false}
+                    showTitle={false}
+                    showByline={false}
+                    responsive={true}
+                    width={50}
+                  />
+                </div>
+              </div>
+
+              <div className={`centerPage ${utilStyles.worksHeading}`}>
+                <h6>Selected</h6>
+                <h1>Work</h1>
+                <Gallery allPosts={allPosts} />
+              </div>
+
+              <div className={`${utilStyles.contactRow} centerPage`}>
+                <IndexContact />
+                <div className={`${utilStyles.form}`}>
+                  <ContactForm showSocials={false} />
+                </div>
+              </div>
             </div>
 
             {/* FOOTER */}
