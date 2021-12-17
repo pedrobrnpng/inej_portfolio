99ef64d000c9fde37bdc78212ef40d26ff33df84
pedrobrnpng <pedrobrandaofl@gmail.com>
2021-12-17 00:19:55 +0000

    ~removed unused imports

--- pages/illustration/index.tsx
+++ pages/illustration/index.tsx
@@ -1,42 +1,18 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../../components/layout'
-import Navbar from '../../components/navbar'
-import Footer from '../../components/footer'
 import Gallery from '../../components/shared-components/gallery'
 import utilStyles from './portfolio.module.css'
-import { useEffect, useState } from 'react'
-import getWindowDimensions from '../../utils/windowUtils'
 import { Post } from '../../types/post'
-import { getAllPosts } from '../../lib/projects'
 import { motion } from 'framer-motion'
 import { createClient } from '../../prismic-config'
 import { convertPrismicToDrawings } from '../../utils/prismicConversions'
+import Prismic from '@prismicio/client'
 
 type Props = {
   allPosts: Post[]
 }
 
 export default function Portfolio({ allPosts }: Props) {
-
-  const [width, setWidth] = useState<number>();
-  const [height, setHeight] = useState<number>();
-
-  useEffect(() => {
-    const { width, height } = getWindowDimensions();
-    setWidth(width);
-    setHeight(height);
-  }, []);
-
-  useEffect(() => {
-    function handleResize() {
-      const { width, height } = getWindowDimensions();
-      setWidth(width);
-      setHeight(height);
-    }
-
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
 
   var uniqueTypes = allPosts.map((post) => post.type.trim())
     .filter((value, index, self) => self.indexOf(value) === index)
